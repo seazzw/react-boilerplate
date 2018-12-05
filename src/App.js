7eee7b6ca231d19e51ca573ae0c8b57490975e21
zhuzhengwang <zhuzhengwang@nanjids.com>
2018-12-05 11:04:39 +0800

    [Dev]安装相应包

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import ReactDOM from "react-dom";
+import './main.css';
+class App extends Component {
+    render() {
+        return (
+            <div>Do Sth</div>
+        );
+    }
+}
+const render = () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+};
+render();
